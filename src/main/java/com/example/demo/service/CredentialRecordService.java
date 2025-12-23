41c05d764771cfbb8da39dfce33ee163d0800cb1
bala-amypo <bala@amypo.in>
2025-12-23 04:57:28 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/CredentialRecordService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.CredentialRecord;
+import java.util.List;
+
+public interface CredentialRecordService {
+    CredentialRecord createCredential(CredentialRecord record);
+    List<CredentialRecord> getAllCredentials();
+}
---
+++ src/main/java/com/example/demo/service/VerificationRequestService.java
@@ -0,0 +1,5 @@
+package com.example.demo.service;
+
+public interface VerificationRequestService {
+    void processVerification(Long id);
+}
